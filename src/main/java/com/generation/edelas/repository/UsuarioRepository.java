18073300b10011c74d7cc74e90dc015a8e4fc43d
douglaspnt <161038707+douglaspnt@users.noreply.github.com>
2024-04-04 12:06:49 -0300

    task_05 esperando aprovacao

---
+++ src/main/java/com/generation/edelas/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.generation.edelas.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.edelas.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	public List<Usuario> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+
+}
