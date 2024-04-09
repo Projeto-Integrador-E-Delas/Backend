aa88f1d267d5baece2825bcec4c9dffcb3ecffcc
barbarabischain <bbischain@gmail.com>
2024-04-09 11:46:10 -0300

    task06 não finalizada

--- src/main/java/com/generation/edelas/repository/UsuarioRepository.java
+++ src/main/java/com/generation/edelas/repository/UsuarioRepository.java
@@ -1,13 +1,12 @@
 package com.generation.edelas.repository;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.generation.edelas.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-	public List<Usuario> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+	public Optional<Usuario> findByEmail(String email);
 
 }
