f2752da0548d8023c1819beb8ea3864363b27655
Eduardo Ferracini <eduardoferracinijunior@gmail.com>
2024-04-15 09:41:07 -0300

    Banco Render

---
+++ src/main/java/com/generation/edelas/configuration/SwaggerConfig.java
@@ -0,0 +1,60 @@
+package com.generation.edelas.configuration;
+
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+
+@Configuration 
+public class SwaggerConfig {
+	
+	@Bean 
+	OpenAPI springBlogPessoalOpenAPI(){ 
+		return new OpenAPI()
+				.info(new Info()
+						.title("E-Delas")
+						.description("Projeto e-Delas – Generation Brasil")
+						.version("v0.0.1")
+						.license(new License()
+								.name("e-Delas")
+								.url("https://github.com/Projeto-Integrador-E-Delas"))
+				.contact(new Contact()
+						.name("e-Delas")
+						.url("https://github.com/Projeto-Integrador-E-Delas")
+						.email("projetointegrador.edelas@gmail.com")))
+				.externalDocs(new ExternalDocumentation()
+						.description("GitHub")
+						.url("https://github.com/Projeto-Integrador-E-Delas"));
+	}
+	
+	@Bean
+	OpenApiCustomizer customerGlobalHeaderOpenApiCustomiser() {
+		
+		return openApi -> {
+			openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+				ApiResponses apiResponses = operation.getResponses();
+				
+				apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
+				apiResponses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
+				apiResponses.addApiResponse("204", createApiResponse("Objeto Excluido!"));
+				apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
+				apiResponses.addApiResponse("401", createApiResponse("Acesso Não Autorizado!"));
+				apiResponses.addApiResponse("403", createApiResponse("Acesso Proibido!"));
+				apiResponses.addApiResponse("404", createApiResponse("Objeto Não Encontrado!"));
+				apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
+			}));
+		};
+	}
+	
+	private ApiResponse createApiResponse(String message) {
+		return new ApiResponse().description(message);
+	}
+
+}
