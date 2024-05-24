a7c343ff660d8264be9815fdd4bdbe4e60bc9684
Victor Yazigi <98867503+oyazigi@users.noreply.github.com>
2024-05-23 23:30:55 -0300

    Update Usuario.java

--- src/main/java/com/generation/edelas/model/Usuario.java
+++ src/main/java/com/generation/edelas/model/Usuario.java
@@ -1,5 +1,6 @@
 package com.generation.edelas.model;
 
+import java.util.Date;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -20,87 +21,109 @@ import jakarta.validation.constraints.Size;
 @Table(name = "tb_usuarios")
 public class Usuario {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@NotBlank(message = "O atributo nome é Obrigatório!")
-	@Size(min = 3, max = 100, message = "O atributo nome deve conter no mínimo 3 e no máximo 100 caracteres")
-	private String nome;
+    @NotBlank(message = "O atributo nome é Obrigatório!")
+    @Size(min = 3, max = 100, message = "O atributo nome deve conter no mínimo 3 e no máximo 100 caracteres")
+    private String nome;
 
-	@Email
-	@NotBlank(message = "O atributo email é Obrigatório!")
-	@Size(min = 5, max = 100, message = "O atributo email deve conter no mínimo 5 e no máximo 100 caracteres")
-	private String email;
+    @Email
+    @NotBlank(message = "O atributo email é Obrigatório!")
+    @Size(min = 5, max = 100, message = "O atributo email deve conter no mínimo 5 e no máximo 100 caracteres")
+    private String email;
 
-	@NotBlank(message = "O atributo cpf é Obrigatório!")
-	@Size(min = 11, max = 11, message = "O atributo cpf deve conter no mínimo 11 e no máximo 11 caracteres")
-	private String cpf;
+    @NotBlank(message = "O atributo cpf é Obrigatório!")
+    @Size(min = 11, max = 11, message = "O atributo cpf deve conter no mínimo 11 e no máximo 11 caracteres")
+    private String cpf;
 
-	@NotBlank(message = "O atributo senha é Obrigatório!")
-	@Size(min = 8, message = "O atributo senha deve conter no mínimo 8 caracteres")
-	private String senha;
+    @NotBlank(message = "O atributo senha é Obrigatório!")
+    @Size(min = 8, message = "O atributo senha deve conter no mínimo 8 caracteres")
+    private String senha;
 
-	private String foto;
+    private String foto;
 
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
-	@JsonIgnoreProperties("usuario")
-	private List<Servico> servico;
+    private Date nascimento;
 
-	public Long getId() {
-		return id;
-	}
+    @NotBlank(message = "O atributo tipo é Obrigatório!")
+    private String tipo;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("usuario")
+    private List<Servico> servico;
 
-	public String getNome() {
-		return nome;
-	}
+    // Getters and Setters
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public String getCpf() {
-		return cpf;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public String getSenha() {
-		return senha;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+    public String getCpf() {
+        return cpf;
+    }
 
-	public String getFoto() {
-		return foto;
-	}
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 
-	public void setFoto(String foto) {
-		this.foto = foto;
-	}
+    public String getSenha() {
+        return senha;
+    }
 
-	public List<Servico> getServico() {
-		return servico;
-	}
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
-	public void setServico(List<Servico> servico) {
-		this.servico = servico;
-	}
+    public String getFoto() {
+        return foto;
+    }
 
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public List<Servico> getServico() {
+        return servico;
+    }
+
+    public void setServico(List<Servico> servico) {
+        this.servico = servico;
+    }
 }
